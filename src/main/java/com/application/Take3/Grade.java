c0cdac99f79605775d43fd740b4001a7f1138c0c
Jaren Mitchell <jaren_mitchell@yahoo.com>
2017-08-01 18:02:48 -0500

    first commit

---
+++ src/main/java/com/application/Take3/Grade.java
@@ -0,0 +1,5 @@
+package com.application.Take3;
+
+public enum Grade {
+    First, Second, Third, Fourth, Fifth, Sixth, Seventh, Eighth, Ninth, Tenth, Eleventh, Twelfth
+}
---
+++ src/main/java/com/application/Take3/Take3Application.java
@@ -0,0 +1,12 @@
+package com.application.Take3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Take3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Take3Application.class, args);
+	}
+}
